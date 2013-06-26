e0145c6035ecb1db17d4b52c280fba1df9f9105b
stackptr <corey@x64.co>
2013-06-26 17:50:42 -0400

    Changing icon on collapse

--- src/js/main.js
+++ src/js/main.js
@@ -2,3 +2,6 @@ $( function() { $("#email-tooltip").tooltip(); } );
 $( function() { $("#irc-tooltip").tooltip(); } );
 $( function() { $("#github-tooltip").tooltip(); } );
 
+$('.accordion-toggle').on('click', function(){
+    $('.accordion-toggle i').toggleClass('icon-chevron-down , icon-chevron-up', 200);
+});
