8386ebd67b32b4c7764cecc33457e88721ea1773
Corey <corey@x64.co>
2013-06-25 23:00:45 -0400

    Added grunt-clean task; outputting to multiple html files from multiple .jade templates

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,19 +3,29 @@ module.exports = function(grunt) {
     // Project configuration
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+        clean: {
+            test: "output"
+        },
 
         // Jade configuration
         jade: {
             compile: {
                 options: {
+                    pretty: true,
                     data: { debug: false }
                 },
-                files: {
-                    "output": ["pages/*.jade"]
-                }
+                files: [{
+                    expand: true,
+                    cwd: "pages",
+                    src: "*.jade",
+                    dest: "output/",
+                    ext: '.html'
+                }]
             }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-jade');
-}
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    return grunt.registerTask("default", ["clean", "jade"] )
+};
