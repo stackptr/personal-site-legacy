da3a284bbab28fccea47d1d63488ba31d40bc4fb
stackptr <corey@x64.co>
2013-06-26 09:43:39 -0400

    added jshint and csslint for validation

--- Gruntfile.js
+++ Gruntfile.js
@@ -30,5 +30,7 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-jade');
     grunt.loadNpmTasks('grunt-contrib-clean');
     //grunt.loadNpmTasks('grunt-contrib-less');
+    //grunt.loadNpmTasks('grunt-contrib-jshint');
+    //grunt.loadNpmTasks('grunt-contrib-csslint');
     return grunt.registerTask("default", ["clean", "jade"] )
 };
