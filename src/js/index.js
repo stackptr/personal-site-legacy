87cc62c0874b6c70bb9f6d15fba75bfbb27051e0
stackptr <corey@x64.co>
2013-06-27 16:37:22 -0400

    Tweaks to js + removing href from collapse link

--- src/js/index.js
+++ src/js/index.js
@@ -3,5 +3,5 @@ $( function() { $("#irc-tooltip").tooltip(); } );
 $( function() { $("#github-tooltip").tooltip(); } );
 
 $('.accordion-toggle').on('click', function(){
-    $('.accordion-toggle i').toggleClass('icon-chevron-down , icon-chevron-up', 200);
+    $('.accordion-toggle i').toggleClass('icon-chevron-down icon-chevron-up', 200);
 });
